daf74057ce099c7ceb948f6f6dea7eeb02abf83d
rakeshravikumar-ML <rakesh.ravikumar.edu@gmail.com>
2025-01-31 16:43:14 -0500

    Update data_preprocessing.py

--- computer_vision/data_preprocessing.py
+++ computer_vision/data_preprocessing.py
@@ -1,1 +1,10 @@
+import tensorflow as tf
 
+def load_and_preprocess_image(filename, img_shape=224, scale=True):
+    """
+    Reads an image file, converts it into a tensor, resizes it, and normalizes pixel values.
+    """
+    img = tf.io.read_file(filename)
+    img = tf.image.decode_jpeg(img)
+    img = tf.image.resize(img, [img_shape, img_shape])
+    return img / 255. if scale else img
